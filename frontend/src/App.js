b43f1710a7d6fd24c968b78e86c23ec8c7285331
hnrqss <hnrq.assuncao@gmail.com>
2019-11-26 14:18:18 -0300

    adding global style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import GlobalStyle from './styles/global'
 import RoutesContainer from './container/RoutesContainer'
 
 function App() {
   return (
     <div>
+      < GlobalStyle />
       <RoutesContainer />
     </div>
   )
