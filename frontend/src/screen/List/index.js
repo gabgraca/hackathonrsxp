63b6f3ec7cc9da7f23684059fa9acb870e1d5d96
Paulo Cezar Lima Zampieri <pczampieri@hotmail.com>
2019-11-27 10:34:25 -0300

    update list api

--- frontend/src/screen/List/index.js
+++ frontend/src/screen/List/index.js
@@ -1,109 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "../../components/Header";
-import api from '../../services/api';
+import api from "../../services/api";
 import { Container } from "./styles";
 
-
 export default function List() {
-
   const [companyList, setCompanyList] = useState([]);
 
   useEffect(() => {
-
-    // api.get()....
-    // Format Date ??
-    // setCompanyList()...
-
-
+    async function loadList() {
+      const response = await api.get("empresas");
+      setCompanyList(response.data);
+    }
+    loadList();
   }, []);
 
   return (
     <>
       <Header />
-      <Container>        
+      <Container>
         <ul>
-           {/**
-            companyList.map(clist => (
+          {companyList.map(clist => (
             <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>{clist.nomedaempresa}</span>
-            <strong>{clist.data}</strong>
-            <p>
-              clist.descricao
-            </p>
-           
-          </li>
-           )) 
-           */}
-          <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>Uber Technologies</span>
-            <p>
-              Empresa de tecnolgia que que faz intermédio de serviço de taxe
-              entre motorista e passageiro.
-            </p>
-            <button>Inscrever-se</button>
-          </li>
-          <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>Uber Technologies</span>
-            <p>
-              Empresa de tecnolgia que que faz intermédio de serviço de taxe
-              entre motorista e passageiro.
-            </p>
-            <button>Inscrever-se</button>
-          </li>
-          <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>Uber Technologies</span>
-            <p>
-              Empresa de tecnolgia que que faz intermédio de serviço de taxe
-              entre motorista e passageiro.
-            </p>
-            <button>Inscrever-se</button>
-          </li>
-          <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>Uber Technologies</span>
-            <p>
-              Empresa de tecnolgia que que faz intermédio de serviço de taxe
-              entre motorista e passageiro.
-            </p>
-            <button>Inscrever-se</button>
-          </li>
-          <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>Uber Technologies</span>
-            <p>
-              Empresa de tecnolgia que que faz intermédio de serviço de taxe
-              entre motorista e passageiro.
-            </p>
-            <button>Inscrever-se</button>
-          </li>
-          <li>
-            <p>
-              02/01/2020
-            </p>
-            <span>Uber Technologies</span>
-            <p>
-              Empresa de tecnolgia que que faz intermédio de serviço de taxe
-              entre motorista e passageiro.
-            </p>
-            <button>Inscrever-se</button>
-          </li>
+              <p>02/01/2020</p>
+              <span>{clist.nome}</span>
+              <strong>{clist.data}</strong>
+              <p>clist.email</p>
+              <button>Inscrever-se</button>
+            </li>
+          ))}
         </ul>
       </Container>
     </>
